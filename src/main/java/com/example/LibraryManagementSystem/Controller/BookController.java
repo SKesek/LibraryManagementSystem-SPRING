dcbcc6dbc3158bdf389f2ede68f5481b6a87f777
Krzysztof Swend <krzysiekswend@gmail.com>
2024-10-23 18:12:07 +0200

    added test endpoint in BookController class

--- src/main/java/com/example/LibraryManagementSystem/Controller/BookController.java
+++ src/main/java/com/example/LibraryManagementSystem/Controller/BookController.java
@@ -1,4 +1,13 @@
 package com.example.LibraryManagementSystem.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class BookController {
+
+    @GetMapping("/")
+    public String welcome(){
+        return "Welcome in Library Management System with SPRING! What do you want to do?";
+    }
 }
